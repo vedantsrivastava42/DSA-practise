c022801282b6c52fa60ac8a8fe4ee54b17eb8b6a
Vedant <vedantsrivastava42@gmail.com>
2023-04-02 23:51:24 +0530

    programs added

---
+++ RECURSION/sum_r.java
@@ -0,0 +1,11 @@
+class sum_r {
+    public static void main(String[] args) {
+        System.out.println(sum(6));
+    }
+
+    static int sum(int n) {
+        if (n == 1)
+            return 1;
+        return n + sum(n - 1);
+    }
+}
