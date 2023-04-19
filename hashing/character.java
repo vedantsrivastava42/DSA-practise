d437c952ed4be69cc1dec1461e66feaba2923333
Vedant <vedantsrivastava42@gmail.com>
2023-04-19 18:35:57 +0530

    new codes

---
+++ hashing/character.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class character{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s;
+        s = sc.next();
+
+        //precompute:
+        int[] hash = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            hash[s.charAt(i) - 'a']++;
+        }
+
+        int q;
+        q = sc.nextInt();
+        while (q-- > 0) {
+            char c;
+            c = sc.next().charAt(0);
+            // fetch:
+            System.out.println(hash[c - 'a']);
+        }
+    }
+}
