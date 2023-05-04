8e3ad06236b8ff1d5467a83c8938a768e9001e5f
Vedant <vedantsrivastava42@gmail.com>
2023-05-04 19:21:56 +0530

    LL added

---
+++ Linked/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
+        list.insertFirst(15);
+        list.insertLast(9);
+        list.insert(100, 3);
+        list.display();
+        // System.out.println(list.deleteFirst());
+        // list.display();
+        // System.out.println(list.deleteLast());
+        // list.display();
+        // System.out.println(list.delete(3));
+        // list.display();
+        System.out.println(list.find(60));
+    }
+}
--- arrays/reverse_array.java
+++ arrays/reverse_array.java
@@ -18,7 +18,6 @@ class reverse_array {
             end--;
         }
     }
-
     static void swap(int[] arr, int index1, int index2) {
         int temp = arr[index1];
         arr[index1] = arr[index2];
